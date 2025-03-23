e05aec4d46acf8f7ef4f8a1d8f85998c63ce11d9
Saad Khan <iamsaadkhattak@gmail.com>
2025-03-23 12:25:58 +0500

    v1.2

--- config.py
+++ config.py
@@ -4,7 +4,11 @@ import os
 
 # Configuration
 ZIMBRA_PATH = "/opt/zimbra/store"
-MALWARE_BAZAAR_API = "https://mb-api.abuse.ch/api/v1/"
+# Remove the old API endpoint
+# MALWARE_BAZAAR_API = "https://mb-api.abuse.ch/api/v1/"
+# Add the new CyberSilo API settings
+CYBERSILO_API_URL = "https://tip.cybersilo.tech/api/ioc/search"
+CYBERSILO_API_TOKEN = "qEdS1VyVJIYnjBgPOa7hX5VawCPqO1Y6"
 MALICIOUS_TAG = "MALICIOUS"
 JSON_LOG_PATH = "/var/log/attachment_hash_malicious.json"
 
